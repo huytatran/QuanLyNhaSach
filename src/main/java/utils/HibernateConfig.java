aaeeb3b9061d19a201528197b6ba229b699ef47c
Tạ Trần Quang Huy <hcgtu1den9@gmail.com>
2026-08-11 16:27:54 +0700

    abc

--- src/main/java/utils/HibernateConfig.java
+++ src/main/java/utils/HibernateConfig.java
@@ -18,9 +18,9 @@ public class HibernateConfig {
         Properties properties = new Properties();
         properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
         properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        properties.put(Environment.URL, "jdbc:sqlserver://sqlserver_db:1433;databaseName=QuanLyNhaSach;encrypt=true;trustServerCertificate=true;");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=QuanLyNhaSach;encrypt=true;trustServerCertificate=true;");
         properties.put(Environment.USER, "sa");
-        properties.put(Environment.PASS, "qưerty123");
+        properties.put(Environment.PASS, "qưẻty123");
         properties.put(Environment.SHOW_SQL, "true");
 
         /*Ht272705
